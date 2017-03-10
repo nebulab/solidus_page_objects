da92e8c7f5616148deb8234165ec184ec82882bd
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-03-10 14:11:40 +0100

    Remove files related to the solidus engine
    This extension provide only page objects for tests/specs so don't need
    the extra files for the solidus applications.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,9 +30,6 @@ require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/factories'
 require 'spree/testing_support/url_helpers'
 
-# Requires factories defined in lib/solidus_page_objects/factories.rb
-require 'solidus_page_objects/factories'
-
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
