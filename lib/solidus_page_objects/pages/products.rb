48caceaeec3f3333071a5c27b56f7ad3fc959d0b
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-04-11 14:30:13 +0200

    Add header, sidebar and product_list to products page

--- lib/solidus_page_objects/pages/products.rb
+++ lib/solidus_page_objects/pages/products.rb
@@ -2,6 +2,10 @@ module SolidusPageObjects
   module Pages
     class Products < SitePrism::Page
       set_url '/products'
+
+      section :header, Sections::Header, '#header'
+      section :sidebar, Sections::Sidebar, '#sidebar'
+      section :product_list, Sections::ProductList, '#products'
     end
   end
 end
