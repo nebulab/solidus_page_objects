28864e1330ff913a229c17e12d81984b44f6fb8f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-06-27 16:23:00 +0200

    Include Checkout component into checkout delivery page

--- lib/solidus_page_objects/pages/checkout/delivery.rb
+++ lib/solidus_page_objects/pages/checkout/delivery.rb
@@ -2,9 +2,12 @@ module SolidusPageObjects
   module Pages
     module Checkout
       class Delivery < SitePrism::Page
+        include Components::Checkout
+
         set_url '/checkout/delivery'
 
         section :header, Sections::Header, '#header'
+
         section :delivery, Sections::Checkout::Delivery, '#checkout_form_delivery'
       end
     end
