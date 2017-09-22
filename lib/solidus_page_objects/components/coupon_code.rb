5bd388331ebf7a666a0d11ed5e2f6d63c33b3baa
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-09-22 14:41:13 +0200

    Fix spree namespace

--- lib/solidus_page_objects/components/coupon_code.rb
+++ lib/solidus_page_objects/components/coupon_code.rb
@@ -4,14 +4,14 @@ module SolidusPageObjects
       def self.included(base)
         base.element :input, "input[name='order[coupon_code]']"
 
-        if Gem::Version.new(Spree.try(:solidus_version) || '1.1') >= Gem::Version.new('1.4')
+        if Gem::Version.new(::Spree.try(:solidus_version) || '1.1') >= Gem::Version.new('1.4')
           base.element :apply_button, "button:first"
         end
       end
 
       def apply(code)
         input.set(code)
-        if Gem::Version.new(Spree.try(:solidus_version) || '1.1') >= Gem::Version.new('1.4')
+        if Gem::Version.new(::Spree.try(:solidus_version) || '1.1') >= Gem::Version.new('1.4')
           apply_button.click
         end
       end
